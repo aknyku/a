c6787b174254b1b8b52c7013b2fc1c39b000ea9d
aknyku <g15932oa@gm.tsuda.ac.jp>
2018-04-12 13:58:36 +0900

    FIRST COMMIT

---
+++ src/helloworld/helloworld1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package helloworld;
+
+/**
+ * @author g15932oa
+ *
+ */
+public class helloworld1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("すごいソフトウェアつくるぞー");
+		// TODO Auto-generated method stub
+
+	}
+
+}
